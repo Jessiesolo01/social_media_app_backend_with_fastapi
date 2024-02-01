514a8c2e2ba8986d2d7eeaa5136776cf26a8832f
Jessica Solomon <jessiesolo01@gmail.com>
2024-02-01 08:53:36 +0100

    changing the world

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import post, user, auth, vote
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
 
-print(settings.database_username)
+# print(settings.database_username)
 # models.Base.metadata.create_all(bind=engine)#alembic now does this for us
 
 app = FastAPI()
@@ -57,6 +57,6 @@ app.include_router(vote.router)
 
 @app.get("/")#path operation more like routing
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!!!"}
 
 
